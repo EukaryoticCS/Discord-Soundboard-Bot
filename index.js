0eca669e9e44c782486aaf2559cd65a9a0dde96d
bsmithNeumont <brsmith@student.neumont.edu>
2022-08-12 21:07:52 -0600

    Added ability to join vc

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-import DiscordJS, { IntentsBitField } from 'discord.js'
+import DiscordJS, { IntentsBitField, Message, messageLink, VoiceChannel } from 'discord.js'
+import { joinVoiceChannel } from '@discordjs/voice'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new DiscordJS.Client({
- intents:[
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
- ]
+	intents:[
+    	IntentsBitField.Flags.Guilds,
+    	IntentsBitField.Flags.GuildMessages,
+    	IntentsBitField.Flags.MessageContent,
+	]
 })
 
 client.on('ready', () => {
-  console.log("Good Morning master")
-})
+	console.log("Good Morning master")
+	//Join vc bit
+	const guild = client.guilds.cache.get("1006328808401555527")
+	const channel = guild.channels.cache.get("1006328808917438547")
+	const connection = joinVoiceChannel({
+    	channelId: channel.id,
+    	guildId: channel.guild.id,
+    	adapterCreator: guild.voiceAdapterCreator,
+	//end join vc bit
+	})
 
-client.on("messageCreate", msg => {
-  if (msg.content === "ping") {
-    msg.reply({content: "Yo momma"});
-  }
+	client.on("messageCreate", (msg) => {
+		if (msg.content === "ping") 
+    		msg.reply({content: "Yo momma"})
+	})
 })
 
 client.login(process.env.TOKEN)

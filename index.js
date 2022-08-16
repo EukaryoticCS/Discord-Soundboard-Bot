21db69bce53cad6223a418edc8164a5fa942210a
MykeOdom <98634442+MykeOdom@users.noreply.github.com>
2022-08-16 16:25:39 -0600

    added prefix

--- index.js
+++ index.js
@@ -30,3 +30,18 @@ client.on('ready', () => {
 })
 
 client.login(process.env.TOKEN)
+
+// Set the prefix 
+const prefix = "!";
+client.on("messageCreate", (message) => {
+	// Exit and stop if it's not there
+	if (!message.content.startsWith(prefix)) return;
+
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send("pong!");
+	} else
+
+	if (message.content.startsWith(`${prefix}yo`)) {
+		message.channel.send("momma!");
+	}
+});
